c58cd41d8c15f65670f1f15aa1d214819c274129
Rafael Costa <rafaelcosta8020@gmail.com>
2024-12-11 21:14:26 -0300

    envio da atividade

---
+++ analise_texto.py
@@ -0,0 +1,8 @@
+def contador_de_palavras(text):
+  return len(text.split())
+
+def contador_de_caracteres(text):
+  return len(text)
+
+def contador_linha(text):
+  return len(text.splitlines())
---
+++ test_analise_texto.py
@@ -0,0 +1,18 @@
+import unittest
+from analise_texto import contador_de_palavras, contador_de_caracteres, contador_linha
+
+class TestTextAnalyzer(unittest.TestCase):
+  def teste_contador_de_palavras(self):
+    text = "Um exemplo de texto."
+    self.assertEqual(contador_de_palavras(text), 4)
+
+  def teste_contador_de_caracteres(self):
+    text = "Outro exemplo."
+    self.assertEqual(contador_de_caracteres(text), 14)
+
+  def teste_contador_linha(self):
+    text = "Linha 1\nLinha 2\nLinha 3"
+    self.assertEqual(contador_linha(text), 3)
+
+if __name__ == "__main__":
+  unittest.main()
